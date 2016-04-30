e6641fdf54ca16b07ad54918231b6ad77f1a1c6c
Georgy Shabunin <georgy.shabunin@gmail.com>
2016-04-30 23:53:23 +0200

    add Google place navigation and refactor

--- gmap-demo/ViewController.swift
+++ gmap-demo/ViewController.swift
@@ -9,35 +9,78 @@
 import UIKit
 import GoogleMaps
 
+class VacationDestination: NSObject{
+
+    let name: String
+    let location: CLLocationCoordinate2D
+    let zoom: Float
+    
+    init(name: String, location: CLLocationCoordinate2D, zoom: Float) {
+        self.name = name
+        self.location = location
+        self.zoom = zoom
+    }
+}
+
 class ViewController: UIViewController {
 
     var mapView: GMSMapView?
+    
+    let destinations = [
+        VacationDestination(name: "SFO Airport", location: CLLocationCoordinate2D(latitude: 37.621338, longitude: -122.378945), zoom: 15),
+        VacationDestination(name: "Embarcadero Station", location: CLLocationCoordinate2D(latitude: 37.793126, longitude: -122.397080), zoom: 15),
+        VacationDestination(name: "Ferry Building", location: CLLocationCoordinate2D(latitude: 37.795793, longitude: -122.393418), zoom: 18)]
+
+    var currentDestinationIndex = -1
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
         GMSServices.provideAPIKey("AIzaSyBNUnjssHDGxAn89_r8jRRT46iDkJUgpso")
- 
-        let camera = GMSCameraPosition.cameraWithLatitude(37.621338, longitude: -122.378945, zoom: 12)
-        mapView = GMSMapView.mapWithFrame(CGRectZero, camera: camera)
-        view = mapView
-        
-        let currentLocation = CLLocationCoordinate2D(latitude: 37.621338, longitude: -122.378945)
-        let marker = GMSMarker(position: currentLocation)
-        marker.title = "SFO Airport"
-        marker.map = mapView
-        
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Next", style: .Plain, target: self, action: "next")
+        
+        setInitialMapCamera()
     
     }
     
     func next(){
-        let nextLocation = CLLocationCoordinate2D(latitude: 37.793126, longitude: -122.397080)
-        mapView?.camera = GMSCameraPosition.cameraWithLatitude( nextLocation.latitude, longitude: nextLocation.longitude, zoom: 15)
+
+        if currentDestinationIndex < (destinations.count - 1){
+            currentDestinationIndex++
+        } else {
+            currentDestinationIndex = 0
+        }
         
-        let marker = GMSMarker(position: nextLocation)
-        marker.title = "Embarcadero Station"
+        setNextMapCamera()
+        
+        
+    }
+    
+    private func setInitialMapCamera(){
+        currentDestinationIndex = 0
+        let currentDestination = destinations[currentDestinationIndex]
+        let camera = GMSCameraPosition.cameraWithTarget(currentDestination.location, zoom: currentDestination.zoom)
+        mapView = GMSMapView.mapWithFrame(CGRectZero, camera: camera)
+        view = mapView
+        
+        setMarker(currentDestination)
+
+    }
+    
+    private func setNextMapCamera() {
+        let currentDestination = destinations[currentDestinationIndex]
+        CATransaction.begin()
+        CATransaction.setValue(2, forKey: kCATransactionAnimationDuration)
+        mapView?.animateToCameraPosition(GMSCameraPosition.cameraWithTarget(currentDestination.location, zoom: currentDestination.zoom))
+        CATransaction.commit()
+        
+        setMarker(currentDestination)
+    }
+    
+    private func setMarker(destination: VacationDestination){
+        let marker = GMSMarker(position: destination.location)
+        marker.title = destination.name
         marker.map = mapView
     }
 
