24eb29bfece0ac1ca5d2403df4f14447689251af
Georgy Shabunin <georgy.shabunin@gmail.com>
2016-04-30 14:17:42 +0200

    adds Google Maps with two-marker navigation

--- gmap-demo/ViewController.swift
+++ gmap-demo/ViewController.swift
@@ -7,17 +7,38 @@
 //
 
 import UIKit
+import GoogleMaps
 
 class ViewController: UIViewController {
 
+    var mapView: GMSMapView?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        GMSServices.provideAPIKey("AIzaSyBNUnjssHDGxAn89_r8jRRT46iDkJUgpso")
+ 
+        let camera = GMSCameraPosition.cameraWithLatitude(37.621338, longitude: -122.378945, zoom: 12)
+        mapView = GMSMapView.mapWithFrame(CGRectZero, camera: camera)
+        view = mapView
+        
+        let currentLocation = CLLocationCoordinate2D(latitude: 37.621338, longitude: -122.378945)
+        let marker = GMSMarker(position: currentLocation)
+        marker.title = "SFO Airport"
+        marker.map = mapView
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Next", style: .Plain, target: self, action: "next")
+    
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func next(){
+        let nextLocation = CLLocationCoordinate2D(latitude: 37.793126, longitude: -122.397080)
+        mapView?.camera = GMSCameraPosition.cameraWithLatitude( nextLocation.latitude, longitude: nextLocation.longitude, zoom: 15)
+        
+        let marker = GMSMarker(position: nextLocation)
+        marker.title = "Embarcadero Station"
+        marker.map = mapView
     }
 
 
