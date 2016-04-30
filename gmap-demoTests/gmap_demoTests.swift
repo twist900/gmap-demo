b1bc544baa5c01ae006b72d17c0dbbf7e64a99a9
Georgy Shabunin <georgy.shabunin@gmail.com>
2016-04-30 13:14:12 +0200

    Initial Commit

---
+++ gmap-demoTests/gmap_demoTests.swift
@@ -0,0 +1,36 @@
+//
+//  gmap_demoTests.swift
+//  gmap-demoTests
+//
+//  Created by Georgy Shabunin on 30/04/16.
+//  Copyright © 2016 Georgy Shabunin. All rights reserved.
+//
+
+import XCTest
+@testable import gmap_demo
+
+class gmap_demoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
